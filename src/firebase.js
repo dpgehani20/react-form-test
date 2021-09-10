442c224317ab0cfa0e16b1f1b292b68d92004b04
dpgehani20 <69641659+dpgehani20@users.noreply.github.com>
2021-09-10 18:29:12 +0530

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
 
 var firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyAu2NKlQGwyRHCeLD_VPL2BOlTL0pbvNgY",
@@ -15,15 +17,3 @@ export { db };
 
 
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAu2NKlQGwyRHCeLD_VPL2BOlTL0pbvNgY",
-//     authDomain: "fir-form-test-3e081.firebaseapp.com",
-//     projectId: "fir-form-test-3e081",
-//     storageBucket: "fir-form-test-3e081.appspot.com",
-//     messagingSenderId: "1042737232384",
-//     appId: "1:1042737232384:web:2b33268c04c03cd33f36e5"
-//   };
-  
-//   // Initialize Firebase
-//   const app = initializeApp(firebaseConfig);
